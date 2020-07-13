787b5f8226de9636c56ec3139f6565fccbcb0b81
oneday <youshihou5@gmail.com>
2020-07-13 23:22:09 +0800

    add LinkedList

---
+++ data-structure/data-structure/List/ArrayList.h
@@ -0,0 +1,14 @@
+//
+//  ArrayList.h
+//  data-structure
+//
+//  Created by Ankui on 7/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef ArrayList_h
+#define ArrayList_h
+
+#include <stdio.h>
+
+#endif /* ArrayList_h */
---
+++ data-structure/data-structure/List/LinkedList.h
@@ -0,0 +1,14 @@
+//
+//  LinkedList.h
+//  data-structure
+//
+//  Created by Ankui on 7/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef LinkedList_h
+#define LinkedList_h
+
+#include <stdio.h>
+
+#endif /* LinkedList_h */
