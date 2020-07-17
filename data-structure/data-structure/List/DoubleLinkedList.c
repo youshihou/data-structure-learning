31245a50939e9b6113adeea898e7e66a0099a66f
oneday <youshihou5@gmail.com>
2020-07-17 22:56:15 +0800

    add DoubleLinkedList

---
+++ data-structure/data-structure/List/DoubleLinkedList.c
@@ -0,0 +1,9 @@
+//
+//  DoubleLinkedList.c
+//  data-structure
+//
+//  Created by Ankui on 7/17/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "DoubleLinkedList.h"
---
+++ data-structure/data-structure/List/DoubleLinkedList.h
@@ -0,0 +1,14 @@
+//
+//  DoubleLinkedList.h
+//  data-structure
+//
+//  Created by Ankui on 7/17/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef DoubleLinkedList_h
+#define DoubleLinkedList_h
+
+#include <stdio.h>
+
+#endif /* DoubleLinkedList_h */
