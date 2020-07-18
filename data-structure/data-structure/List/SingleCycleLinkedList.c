87c37b818a2198df45d0d72313848725e7379851
oneday <youshihou5@gmail.com>
2020-07-18 11:20:41 +0800

    add SingleLinkedList

---
+++ data-structure/data-structure/List/SingleCycleLinkedList.c
@@ -0,0 +1,9 @@
+//
+//  SingleCycleLinkedList.c
+//  data-structure
+//
+//  Created by Ankui on 7/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "SingleCycleLinkedList.h"
---
+++ data-structure/data-structure/List/SingleCycleLinkedList.h
@@ -0,0 +1,14 @@
+//
+//  SingleCycleLinkedList.h
+//  data-structure
+//
+//  Created by Ankui on 7/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef SingleCycleLinkedList_h
+#define SingleCycleLinkedList_h
+
+#include <stdio.h>
+
+#endif /* SingleCycleLinkedList_h */
