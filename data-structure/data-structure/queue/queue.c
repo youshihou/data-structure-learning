3da50fb456eb59f44fbe139495ee038f192f8994
oneday <youshihou5@gmail.com>
2020-07-18 22:40:17 +0800

    add queue

---
+++ data-structure/data-structure/queue/queue.c
@@ -0,0 +1,9 @@
+//
+//  queue.c
+//  data-structure
+//
+//  Created by Ankui on 7/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "queue.h"
---
+++ data-structure/data-structure/queue/queue.h
@@ -0,0 +1,14 @@
+//
+//  queue.h
+//  data-structure
+//
+//  Created by Ankui on 7/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef queue_h
+#define queue_h
+
+#include <stdio.h>
+
+#endif /* queue_h */
