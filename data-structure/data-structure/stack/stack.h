3c99612ebfa7ebbf0e04eba65b13698e962a9a2b
oneday <youshihou5@gmail.com>
2020-07-18 17:23:07 +0800

    add stack

---
+++ data-structure/data-structure/stack/stack.h
@@ -0,0 +1,14 @@
+//
+//  stack.h
+//  data-structure
+//
+//  Created by Ankui on 7/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef stack_h
+#define stack_h
+
+#include <stdio.h>
+
+#endif /* stack_h */
