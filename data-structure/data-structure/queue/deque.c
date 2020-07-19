a716fb67967dbcfdf587abf840410890d25ab7b8
oneday <youshihou5@gmail.com>
2020-07-19 10:28:11 +0800

    add deque

---
+++ data-structure/data-structure/queue/deque.c
@@ -0,0 +1,9 @@
+//
+//  deque.c
+//  data-structure
+//
+//  Created by Ankui on 7/19/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "deque.h"
---
+++ data-structure/data-structure/queue/deque.h
@@ -0,0 +1,14 @@
+//
+//  deque.h
+//  data-structure
+//
+//  Created by Ankui on 7/19/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef deque_h
+#define deque_h
+
+#include <stdio.h>
+
+#endif /* deque_h */
