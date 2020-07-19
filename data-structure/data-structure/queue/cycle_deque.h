b8af530aacd50075244eb1a69369fc14053bfa63
oneday <youshihou5@gmail.com>
2020-07-19 14:02:49 +0800

    add cycle_deque

---
+++ data-structure/data-structure/queue/cycle_deque.h
@@ -0,0 +1,14 @@
+//
+//  cycle_deque.h
+//  data-structure
+//
+//  Created by Ankui on 7/19/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef cycle_deque_h
+#define cycle_deque_h
+
+#include <stdio.h>
+
+#endif /* cycle_deque_h */
