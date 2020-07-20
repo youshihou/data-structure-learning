3efd42ec30e77ce8d67e2efdc83858e7fb9a3fc9
oneday <youshihou5@gmail.com>
2020-07-20 23:11:36 +0800

    add binary_search_tree

---
+++ data-structure/data-structure/tree/binary_search_tree.c
@@ -0,0 +1,9 @@
+//
+//  binary_search_tree.c
+//  data-structure
+//
+//  Created by Ankui on 7/20/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "binary_search_tree.h"
---
+++ data-structure/data-structure/tree/binary_search_tree.h
@@ -0,0 +1,14 @@
+//
+//  binary_search_tree.h
+//  data-structure
+//
+//  Created by Ankui on 7/20/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef binary_search_tree_h
+#define binary_search_tree_h
+
+#include <stdio.h>
+
+#endif /* binary_search_tree_h */
