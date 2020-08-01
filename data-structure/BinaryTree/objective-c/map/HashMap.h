c956b7a7e99db0ce2e84cef9df35086d82ac3cf5
oneday <youshihou5@gmail.com>
2020-08-01 17:42:37 +0800

    add HashMap

---
+++ data-structure/BinaryTree/objective-c/map/HashMap.h
@@ -0,0 +1,17 @@
+//
+//  HashMap.h
+//  BinaryTree
+//
+//  Created by Ankui on 7/31/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HashMap : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/BinaryTree/objective-c/map/HashMap.m
@@ -0,0 +1,13 @@
+//
+//  HashMap.m
+//  BinaryTree
+//
+//  Created by Ankui on 7/31/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "HashMap.h"
+
+@implementation HashMap
+
+@end
