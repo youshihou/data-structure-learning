b9bcd59cf89eae8f4c0457ba567cc837e383f7b5
oneday <youshihou5@gmail.com>
2020-08-02 14:40:56 +0800

    add LinkedHashMap

---
+++ data-structure/BinaryTree/objective-c/hashmap/LinkedHashMap.h
@@ -0,0 +1,17 @@
+//
+//  LinkedHashMap.h
+//  BinaryTree
+//
+//  Created by Ankui on 8/2/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "HashMap.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LinkedHashMap : HashMap
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/BinaryTree/objective-c/hashmap/LinkedHashMap.m
@@ -0,0 +1,13 @@
+//
+//  LinkedHashMap.m
+//  BinaryTree
+//
+//  Created by Ankui on 8/2/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "LinkedHashMap.h"
+
+@implementation LinkedHashMap
+
+@end
