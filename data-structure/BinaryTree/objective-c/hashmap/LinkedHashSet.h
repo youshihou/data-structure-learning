795a4ed11e7fa7513c68618d928f9f6d2b0156e2
oneday <youshihou5@gmail.com>
2020-08-02 14:52:03 +0800

    add LinkedHashSet

---
+++ data-structure/BinaryTree/objective-c/hashmap/LinkedHashSet.h
@@ -0,0 +1,17 @@
+//
+//  LinkedHashSet.h
+//  BinaryTree
+//
+//  Created by Ankui on 8/2/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "LinkedHashMap.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LinkedHashSet : LinkedHashMap
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/BinaryTree/objective-c/hashmap/LinkedHashSet.m
@@ -0,0 +1,13 @@
+//
+//  LinkedHashSet.m
+//  BinaryTree
+//
+//  Created by Ankui on 8/2/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "LinkedHashSet.h"
+
+@implementation LinkedHashSet
+
+@end
