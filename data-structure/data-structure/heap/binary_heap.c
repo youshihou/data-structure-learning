5feb71d7f716eb997d8bab96ce5006e2760d5046
oneday <youshihou5@gmail.com>
2020-08-02 22:43:20 +0800

    add binary_heap

---
+++ data-structure/data-structure/heap/binary_heap.c
@@ -0,0 +1,32 @@
+//
+//  binary_heap.c
+//  data-structure
+//
+//  Created by Ankui on 8/2/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "binary_heap.h"
+
+
+int heap_size(void) {
+    
+}
+bool heap_isEmpty(void) {
+    
+}
+void heap_clear(void) {
+    
+}
+void heap_add(int element) {
+    
+}
+int heap_get(void) {
+    
+}
+int heap_remove(void) {
+    
+}
+int heap_replace(int element) {
+    
+}
---
+++ data-structure/data-structure/heap/binary_heap.h
@@ -0,0 +1,22 @@
+//
+//  binary_heap.h
+//  data-structure
+//
+//  Created by Ankui on 8/2/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef binary_heap_h
+#define binary_heap_h
+
+#include "common.h"
+
+int heap_size(void);
+bool heap_isEmpty(void);
+void heap_clear(void);
+void heap_add(int);
+int heap_get(void);
+int heap_remove(void);
+int heap_replace(int);
+
+#endif /* binary_heap_h */
