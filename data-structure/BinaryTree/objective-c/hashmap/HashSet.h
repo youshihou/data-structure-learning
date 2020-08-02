34c8eb9efe83bc7b75db94d2dba1dfcbb5183cb7
oneday <youshihou5@gmail.com>
2020-08-02 14:48:50 +0800

    add HashSet

---
+++ data-structure/BinaryTree/objective-c/hashmap/HashSet.h
@@ -0,0 +1,17 @@
+//
+//  HashSet.h
+//  BinaryTree
+//
+//  Created by Ankui on 8/2/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HashSet : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/BinaryTree/objective-c/hashmap/HashSet.m
@@ -0,0 +1,13 @@
+//
+//  HashSet.m
+//  BinaryTree
+//
+//  Created by Ankui on 8/2/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "HashSet.h"
+
+@implementation HashSet
+
+@end
