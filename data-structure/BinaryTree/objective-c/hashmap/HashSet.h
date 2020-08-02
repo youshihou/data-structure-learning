2619ca9cc53d1ec368d5b6233a6bf2d917c71077
oneday <youshihou5@gmail.com>
2020-08-02 14:48:57 +0800

    add HashSet

--- data-structure/BinaryTree/objective-c/hashmap/HashSet.h
+++ data-structure/BinaryTree/objective-c/hashmap/HashSet.h
@@ -6,12 +6,19 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "HashMap.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface HashSet : NSObject
-
++ (instancetype)set;
+- (NSUInteger)size;
+- (BOOL)isEmpty;
+- (void)clear;
+- (BOOL)contains:(id _Nullable)element;
+- (void)add:(id _Nullable)element;
+- (void)remove:(id _Nullable)element;
+- (void)traversal:(struct HashMapVisitor * _Nullable)visitor;
 @end
 
 NS_ASSUME_NONNULL_END
--- data-structure/BinaryTree/objective-c/hashmap/HashSet.m
+++ data-structure/BinaryTree/objective-c/hashmap/HashSet.m
@@ -8,6 +8,40 @@
 
 #import "HashSet.h"
 
-@implementation HashSet
+@interface HashSet () {
+    HashMap *_map;
+}
+@end
 
+@implementation HashSet
+- (instancetype)init {
+    if (self = [super init]) {
+        _map = [HashMap map];
+    }
+    return self;
+}
++ (instancetype)set {
+    return [[self alloc] init];
+}
+- (NSUInteger)size {
+    return [_map size];
+}
+- (BOOL)isEmpty {
+    return [_map isEmpty];
+}
+- (void)clear {
+    [_map clear];
+}
+- (BOOL)contains:(id)element {
+    return [_map containsKey:element];
+}
+- (void)add:(id)element {
+    [_map put:element value:nil];
+}
+- (void)remove:(id)element {
+    [_map remove:element];
+}
+- (void)traversal:(struct HashMapVisitor *)visitor {
+    [_map traversal:visitor];
+}
 @end
