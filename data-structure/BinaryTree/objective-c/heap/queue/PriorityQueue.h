727d8318a70dd71a91b17587ba48d067fd025b28
oneday <youshihou5@gmail.com>
2020-08-05 22:39:22 +0800

    add PriorityQueue

---
+++ data-structure/BinaryTree/objective-c/heap/queue/PriorityQueue.h
@@ -0,0 +1,17 @@
+//
+//  PriorityQueue.h
+//  BinaryTree
+//
+//  Created by Ankui on 8/5/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PriorityQueue : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/BinaryTree/objective-c/heap/queue/PriorityQueue.m
@@ -0,0 +1,13 @@
+//
+//  PriorityQueue.m
+//  BinaryTree
+//
+//  Created by Ankui on 8/5/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "PriorityQueue.h"
+
+@implementation PriorityQueue
+
+@end
