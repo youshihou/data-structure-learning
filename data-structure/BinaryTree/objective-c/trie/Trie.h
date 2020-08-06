bfc8d9da33955e1bbdf9a3a28f556d1d5bb5b0a5
oneday <youshihou5@gmail.com>
2020-08-06 22:14:33 +0800

    add Trie

---
+++ data-structure/BinaryTree/objective-c/trie/Trie.h
@@ -0,0 +1,17 @@
+//
+//  Trie.h
+//  BinaryTree
+//
+//  Created by Ankui on 8/6/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Trie : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/BinaryTree/objective-c/trie/Trie.m
@@ -0,0 +1,13 @@
+//
+//  Trie.m
+//  BinaryTree
+//
+//  Created by Ankui on 8/6/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "Trie.h"
+
+@implementation Trie
+
+@end
