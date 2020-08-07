81236f06aa66cfd9238d239d64f0ce2280040d8f
oneday <youshihou5@gmail.com>
2020-08-07 20:43:41 +0800

    add Trie

--- data-structure/BinaryTree/objective-c/trie/Trie.m
+++ data-structure/BinaryTree/objective-c/trie/Trie.m
@@ -16,12 +16,6 @@
 }
 @end
 @implementation TrieNode
-- (NSMutableDictionary *)getChild {
-    if (!_child) {
-        _child = [NSMutableDictionary dictionary];
-    }
-    return _child;
-}
 @end
 
 @interface Trie () {
@@ -31,13 +25,6 @@
 @end
 
 @implementation Trie
-- (instancetype)init {
-    if (self = [super init]) {
-        _root = [[TrieNode alloc] init];
-    }
-    return self;
-}
-
 + (instancetype)trie {
     return [[self alloc] init];
 }
@@ -50,28 +37,34 @@
 - (void)clear {
     if (_size == 0) { return; }
     _size = 0;
-    [_root->_child removeAllObjects];
+    _root = nil;
 }
 - (id)get:(NSString *)key {
     TrieNode *node = [self _node:key];
-    return node ? node->_value : nil;
+    return node && node->_word ? node->_value : nil;
 }
 - (BOOL)contains:(NSString *)key {
-    return [self _node:key] != nil;
+    TrieNode *node = [self _node:key];
+    return node && node->_word;
 
 }
 - (id)addKey:(NSString *)key value:(id)value {
     [self _keyCheck:key];
+    if (!_root) {
+        _root = [[TrieNode alloc] init];
+    }
     
     TrieNode *node = _root;
     NSUInteger len = key.length;
     for (NSUInteger i = 0; i < len; i++) {
         unichar c = [key characterAtIndex:i];
         NSString *k = [NSString stringWithCharacters:&c length:1];
-        TrieNode *childNode = [node getChild][k];
+        BOOL empty = node->_child == nil;
+        TrieNode *childNode = empty ? nil : node->_child[k];
         if (!childNode) {
             childNode = [[TrieNode alloc] init];
-            [node getChild][k] = childNode;
+            node->_child = empty ? [NSMutableDictionary dictionary] : node->_child;
+            node->_child[k] = childNode;
         }
         node = childNode;
     }
@@ -91,17 +84,7 @@
     return nil;
 }
 - (BOOL)starsWith:(NSString *)prefix {
-    [self _keyCheck:prefix];
-    
-    TrieNode *node = _root;
-    NSUInteger len = prefix.length;
-    for (NSUInteger i = 0; i < len; i++) {
-        unichar c = [prefix characterAtIndex:i];
-        NSString *k = [NSString stringWithCharacters:&c length:1];
-        node = [node getChild][k];
-        if (!node) { return NO; }
-    }
-    return YES;
+    return [self _node:prefix] != nil;
 }
 
 
@@ -111,12 +94,12 @@
     TrieNode *node = _root;
     NSUInteger len = key.length;
     for (NSUInteger i = 0; i < len; i++) {
+        if (!node || !node->_child || node->_child.count == 0) { return nil; }
         unichar c = [key characterAtIndex:i];
         NSString *k = [NSString stringWithCharacters:&c length:1];
-        node = [node getChild][k];
-        if (!node) { return nil; }
+        node = node->_child[k];
     }
-    return node->_word ? node : nil;
+    return node;
 }
 - (void)_keyCheck:(NSString *)key {
     if (key.length == 0) {
