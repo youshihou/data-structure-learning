008a8e58cef996a4e302f10edef4ec98b48a26c6
oneday <youshihou5@gmail.com>
2020-08-09 20:38:27 +0800

    add BinaryTree

---
+++ data-structure/LinkedList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LinkedList
+//
+//  Created by Ankui on 8/9/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
