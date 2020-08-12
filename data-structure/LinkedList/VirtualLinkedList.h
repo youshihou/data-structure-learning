8140a93581508813c526aae92c84ecc940a26d95
oneday <youshihou5@gmail.com>
2020-08-12 21:33:21 +0800

    add VirtualLinkedList

---
+++ data-structure/LinkedList/VirtualLinkedList.h
@@ -0,0 +1,17 @@
+//
+//  VirtualLinkedList.h
+//  LinkedList
+//
+//  Created by Ankui on 8/12/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "List.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VirtualLinkedList : List
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/LinkedList/VirtualLinkedList.m
@@ -0,0 +1,13 @@
+//
+//  VirtualLinkedList.m
+//  LinkedList
+//
+//  Created by Ankui on 8/12/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "VirtualLinkedList.h"
+
+@implementation VirtualLinkedList
+
+@end
