f6b22e6e01a0565af44b9470926f820aa2056e9a
oneday <youshihou5@gmail.com>
2020-08-13 21:40:40 +0800

    add DLinkedList

---
+++ data-structure/LinkedList/DList/DLinkedList.h
@@ -0,0 +1,17 @@
+//
+//  DLinkedList.h
+//  LinkedList
+//
+//  Created by Ankui on 8/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "List.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DLinkedList : List
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/LinkedList/DList/DLinkedList.m
@@ -0,0 +1,13 @@
+//
+//  DLinkedList.m
+//  LinkedList
+//
+//  Created by Ankui on 8/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "DLinkedList.h"
+
+@implementation DLinkedList
+
+@end
