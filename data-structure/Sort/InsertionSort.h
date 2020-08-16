0490a92e2a643a1349fdb3f5df5cad87005d19be
oneday <youshihou5@gmail.com>
2020-08-16 17:19:03 +0800

    add InsertionSort

---
+++ data-structure/Sort/InsertionSort.h
@@ -0,0 +1,17 @@
+//
+//  Insertion.h
+//  Sort
+//
+//  Created by Ankui on 8/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "Sort.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InsertionSort : Sort
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/Sort/InsertionSort.m
@@ -0,0 +1,15 @@
+//
+//  Insertion.m
+//  Sort
+//
+//  Created by Ankui on 8/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "InsertionSort.h"
+
+@implementation InsertionSort
+- (void)sorting {
+    
+}
+@end
