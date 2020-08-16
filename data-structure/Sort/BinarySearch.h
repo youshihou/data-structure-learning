abf1efaa501a7d8a84809275ffed33ebb06c4883
oneday <youshihou5@gmail.com>
2020-08-16 17:50:20 +0800

    add BinarySearch

---
+++ data-structure/Sort/BinarySearch.h
@@ -0,0 +1,17 @@
+//
+//  BinarySearch.h
+//  Sort
+//
+//  Created by Ankui on 8/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BinarySearch : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/Sort/BinarySearch.m
@@ -0,0 +1,13 @@
+//
+//  BinarySearch.m
+//  Sort
+//
+//  Created by Ankui on 8/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "BinarySearch.h"
+
+@implementation BinarySearch
+
+@end
