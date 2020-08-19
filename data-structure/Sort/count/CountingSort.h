7cb68a49fb469dbdec77503b223c7beab202be09
oneday <youshihou5@gmail.com>
2020-08-19 22:03:54 +0800

    add CountingSort

---
+++ data-structure/Sort/count/CountingSort.h
@@ -0,0 +1,17 @@
+//
+//  CountingSort.h
+//  Sort
+//
+//  Created by Ankui on 8/19/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "Sort.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CountingSort : Sort
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/Sort/count/CountingSort.m
@@ -0,0 +1,13 @@
+//
+//  CountingSort.m
+//  Sort
+//
+//  Created by Ankui on 8/19/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "CountingSort.h"
+
+@implementation CountingSort
+
+@end
