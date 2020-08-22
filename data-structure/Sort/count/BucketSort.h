ab11b115231e0435e86b1f3ca3e531d4f73ca8ce
oneday <youshihou5@gmail.com>
2020-08-22 13:29:27 +0800

    add BucketSort

---
+++ data-structure/Sort/count/BucketSort.h
@@ -0,0 +1,17 @@
+//
+//  BucketSort.h
+//  Sort
+//
+//  Created by Ankui on 8/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "Sort.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BucketSort : Sort
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/Sort/count/BucketSort.m
@@ -0,0 +1,13 @@
+//
+//  BucketSort.m
+//  Sort
+//
+//  Created by Ankui on 8/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "BucketSort.h"
+
+@implementation BucketSort
+
+@end
