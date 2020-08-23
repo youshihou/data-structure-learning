b71b82e7da35600af28550bcf8fd123f5558022d
oneday <youshihou5@gmail.com>
2020-08-23 21:04:30 +0800

    add QuickUnion

---
+++ data-structure/UnionFind/quick-union/QuickUnion.h
@@ -0,0 +1,17 @@
+//
+//  QuickUnion.h
+//  UnionFind
+//
+//  Created by Ankui on 8/23/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "UnionFind.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QuickUnion : UnionFind
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/UnionFind/quick-union/QuickUnion.m
@@ -0,0 +1,13 @@
+//
+//  QuickUnion.m
+//  UnionFind
+//
+//  Created by Ankui on 8/23/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "QuickUnion.h"
+
+@implementation QuickUnion
+
+@end
