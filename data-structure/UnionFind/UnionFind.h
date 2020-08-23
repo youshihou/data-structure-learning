19ce077a5e1847ef6a3b041daff962b245938a9b
oneday <youshihou5@gmail.com>
2020-08-23 20:32:57 +0800

    add UnionFind

---
+++ data-structure/UnionFind/UnionFind.h
@@ -0,0 +1,17 @@
+//
+//  UnionFind.h
+//  UnionFind
+//
+//  Created by Ankui on 8/23/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UnionFind : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/UnionFind/UnionFind.m
@@ -0,0 +1,13 @@
+//
+//  UnionFind.m
+//  UnionFind
+//
+//  Created by Ankui on 8/23/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "UnionFind.h"
+
+@implementation UnionFind
+
+@end
