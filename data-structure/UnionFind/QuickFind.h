86e22af75f692738104383d65d620845e0700dbe
oneday <youshihou5@gmail.com>
2020-08-23 20:42:52 +0800

    add QuickFind

---
+++ data-structure/UnionFind/QuickFind.h
@@ -0,0 +1,17 @@
+//
+//  QuickFind.h
+//  UnionFind
+//
+//  Created by Ankui on 8/23/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "UnionFind.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QuickFind : UnionFind
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/UnionFind/QuickFind.m
@@ -0,0 +1,13 @@
+//
+//  QuickFind.m
+//  UnionFind
+//
+//  Created by Ankui on 8/23/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "QuickFind.h"
+
+@implementation QuickFind
+
+@end
