02e9f0dfe1a17f5f51e32a629a9f6401eb433dad
oneday <youshihou5@gmail.com>
2020-08-24 21:13:25 +0800

    add QuickUnionRank

---
+++ data-structure/UnionFind/quick-union/QuickUnionRank.h
@@ -0,0 +1,17 @@
+//
+//  QuickUnionRank.h
+//  UnionFind
+//
+//  Created by Ankui on 8/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "QuickUnion.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QuickUnionRank : QuickUnion
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/UnionFind/quick-union/QuickUnionRank.m
@@ -0,0 +1,13 @@
+//
+//  QuickUnionRank.m
+//  UnionFind
+//
+//  Created by Ankui on 8/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "QuickUnionRank.h"
+
+@implementation QuickUnionRank
+
+@end
