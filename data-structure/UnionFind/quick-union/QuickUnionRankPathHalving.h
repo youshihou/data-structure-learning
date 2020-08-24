48680a5aa711dec99e6ef4fd78beed665850cc03
oneday <youshihou5@gmail.com>
2020-08-24 21:48:52 +0800

    add QuickUnionRankPathHalving

---
+++ data-structure/UnionFind/quick-union/QuickUnionRankPathHalving.h
@@ -0,0 +1,17 @@
+//
+//  QuickUnionRankPathHalving.h
+//  UnionFind
+//
+//  Created by Ankui on 8/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "QuickUnionRank.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QuickUnionRankPathHalving : QuickUnionRank
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/UnionFind/quick-union/QuickUnionRankPathHalving.m
@@ -0,0 +1,13 @@
+//
+//  QuickUnionRankPathHalving.m
+//  UnionFind
+//
+//  Created by Ankui on 8/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "QuickUnionRankPathHalving.h"
+
+@implementation QuickUnionRankPathHalving
+
+@end
