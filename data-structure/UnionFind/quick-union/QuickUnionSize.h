9b5745be2f09f6618a25e840cd2a028998c91ef6
oneday <youshihou5@gmail.com>
2020-08-24 20:29:49 +0800

    add QuickUnionSize

---
+++ data-structure/UnionFind/quick-union/QuickUnionSize.h
@@ -0,0 +1,17 @@
+//
+//  QuickUnionSize.h
+//  UnionFind
+//
+//  Created by Ankui on 8/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "UnionFind.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QuickUnionSize : UnionFind
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/UnionFind/quick-union/QuickUnionSize.m
@@ -0,0 +1,13 @@
+//
+//  QuickUnionSize.m
+//  UnionFind
+//
+//  Created by Ankui on 8/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "QuickUnionSize.h"
+
+@implementation QuickUnionSize
+
+@end
