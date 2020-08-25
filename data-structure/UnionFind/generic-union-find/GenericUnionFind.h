5c6166ae43415deed0af2538d8694fe69b08f931
oneday <youshihou5@gmail.com>
2020-08-25 21:00:12 +0800

    add GenericUnionFind

---
+++ data-structure/UnionFind/generic-union-find/GenericUnionFind.h
@@ -0,0 +1,17 @@
+//
+//  GenericUnionFind.h
+//  UnionFind
+//
+//  Created by Ankui on 8/25/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GenericUnionFind : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/UnionFind/generic-union-find/GenericUnionFind.m
@@ -0,0 +1,13 @@
+//
+//  GenericUnionFind.m
+//  UnionFind
+//
+//  Created by Ankui on 8/25/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "GenericUnionFind.h"
+
+@implementation GenericUnionFind
+
+@end
