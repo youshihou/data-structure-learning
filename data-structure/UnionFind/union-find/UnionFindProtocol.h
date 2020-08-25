d71f98b442db2c3912d2a80f3c2753e0ee309601
oneday <youshihou5@gmail.com>
2020-08-25 21:16:58 +0800

    add UnionFindProtocol

---
+++ data-structure/UnionFind/union-find/UnionFindProtocol.h
@@ -0,0 +1,17 @@
+//
+//  UnionFindProtocol.h
+//  UnionFind
+//
+//  Created by Ankui on 8/25/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol UnionFindProtocol <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
