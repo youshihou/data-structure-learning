060121a6ac547bb863969f04c3ccedb8bd4e43db
oneday <youshihou5@gmail.com>
2020-08-26 21:44:59 +0800

    add UnionFindProtocol

---
+++ data-structure/UnionFind/union-find/Test/Test/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Test
+//
+//  Created by Ankui on 8/26/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ data-structure/UnionFind/union-find/Test/Test/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Ankui on 8/26/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
