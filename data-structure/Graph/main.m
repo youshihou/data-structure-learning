ff1cdd45e84387a1711ee82e6a0d175a42f5e120
oneday <youshihou5@gmail.com>
2020-09-01 20:47:00 +0800

    add ListGraph

--- data-structure/Graph/main.m
+++ data-structure/Graph/main.m
@@ -8,18 +8,41 @@
 
 #import "ListGraph.h"
 
+void testListGraph(void) {
+    ListGraph *graph = [ListGraph graph];
+    [graph addEdge:@"V1" to:@"V0" weight:@9];
+    [graph addEdge:@"V1" to:@"V2" weight:@3];
+    [graph addEdge:@"V2" to:@"V0" weight:@2];
+    [graph addEdge:@"V2" to:@"V3" weight:@5];
+    [graph addEdge:@"V3" to:@"V4" weight:@1];
+    [graph addEdge:@"V0" to:@"V4" weight:@6];
+//    [graph removeEdge:@"V0" to:@"V4"];
+    [graph removeVertex:@"V0"];
+    [graph print];
+}
+void testListGraph2(void) {
+    ListGraph *graph = [ListGraph graph];
+    [graph addEdge:@"V0" to:@"V1"];
+    [graph addEdge:@"V1" to:@"V0"];
+    
+    [graph addEdge:@"V0" to:@"V2"];
+    [graph addEdge:@"V2" to:@"V0"];
+    
+    [graph addEdge:@"V0" to:@"V3"];
+    [graph addEdge:@"V3" to:@"V0"];
+    
+    [graph addEdge:@"V2" to:@"V1"];
+    [graph addEdge:@"V1" to:@"V2"];
+    
+    [graph addEdge:@"V2" to:@"V3"];
+    [graph addEdge:@"V3" to:@"V2"];
+    [graph print];
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        ListGraph *graph = [ListGraph graph];
-        [graph addEdge:@"V1" to:@"V0" weight:@9];
-        [graph addEdge:@"V1" to:@"V2" weight:@3];
-        [graph addEdge:@"V2" to:@"V0" weight:@2];
-        [graph addEdge:@"V2" to:@"V3" weight:@5];
-        [graph addEdge:@"V3" to:@"V4" weight:@1];
-        [graph addEdge:@"V0" to:@"V4" weight:@6];
-//        [graph removeEdge:@"V0" to:@"V4"];
-        [graph removeVertex:@"V0"];
-        [graph print];
+//        testListGraph();
+        testListGraph2();
     }
     return 0;
 }
