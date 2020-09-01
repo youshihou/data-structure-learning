49757114e834fab6b0b9e2d10a9e17a4d47a93b2
oneday <youshihou5@gmail.com>
2020-09-01 22:05:28 +0800

    add bfs

--- data-structure/Graph/main.m
+++ data-structure/Graph/main.m
@@ -40,11 +40,97 @@ void testListGraph2(void) {
     [graph print];
 }
 
+@interface GraphData : NSObject
+
+@end
+@implementation GraphData
++ (NSArray *)BFS_01 {
+    return @[@[@"A", @"B"], @[@"A", @"F"],
+             @[@"B", @"C"], @[@"B", @"I"], @[@"B", @"G"],
+             @[@"C", @"I"], @[@"C", @"D"],
+             @[@"D", @"I"], @[@"D", @"G"], @[@"D", @"E"], @[@"D", @"H"],
+             @[@"E", @"H"], @[@"E", @"F"],
+             @[@"F", @"G"],
+             @[@"G", @"H"],
+    ];
+}
++ (NSArray *)BFS_02 {
+    return @[@[@0, @1], @[@0, @4],
+             @[@1, @2],
+             @[@2, @0], @[@2, @4], @[@2, @5],
+             @[@3, @1],
+             @[@4, @6], @[@4, @7],
+             @[@5, @3], @[@5, @7],
+             @[@6, @2], @[@6, @7],
+    ];
+}
++ (NSArray *)BFS_03 {
+    return @[@[@0, @2], @[@0, @3],
+             @[@1, @2], @[@1, @3], @[@1, @6],
+             @[@2, @4],
+             @[@3, @7],
+             @[@4, @6],
+             @[@5, @6],
+             @[@6, @7],
+    ];
+}
++ (NSArray *)BFS_04 {
+    return @[@[@1, @2], @[@1, @3], @[@1, @5],
+             @[@2, @0],
+             @[@3, @5],
+             @[@5, @6], @[@5, @7],
+             @[@6, @2],
+             @[@7, @6],
+    ];
+}
+@end
+
+ListGraph* testDirectedGraph(NSArray *data) {
+    ListGraph *graph = [ListGraph graph];
+    for (NSArray *list in data) {
+        if (list.count == 1) {
+            [graph addVertex:list.firstObject];
+        } else if (list.count == 2) {
+            [graph addEdge:list.firstObject to:list.lastObject];
+        } else if (list.count == 3) {
+            [graph addEdge:list.firstObject to:list[1] weight:list.lastObject];
+        }
+    }
+    return graph;
+}
+ListGraph* testUndirectedGraph(NSArray *data) {
+    ListGraph *graph = [ListGraph graph];
+    for (NSArray *list in data) {
+        if (list.count == 1) {
+            [graph addVertex:list.firstObject];
+        } else if (list.count == 2) {
+            id from = list.firstObject;
+            id to = list.lastObject;
+            [graph addEdge:from to:to];
+            [graph addEdge:to to:from];
+        } else if (list.count == 3) {
+            id from = list.firstObject;
+            id to = list[1];
+            id weight = list.lastObject;
+            [graph addEdge:from to:to weight:weight];
+            [graph addEdge:to to:from weight:weight];
+        }
+    }
+    return graph;
+}
+
+void testBfs() {
+//    ListGraph *graph = testUndirectedGraph([GraphData BFS_01]);
+//    [graph bfs:@"A"];
+    ListGraph *graph = testDirectedGraph([GraphData BFS_02]);
+    [graph bfs:@5];
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        testListGraph();
+//        testListGraph();
 //        testListGraph2();
-        
+        testBfs();
     }
     return 0;
 }
