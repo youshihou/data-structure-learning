bdb90dc236d2bd1c142251fc3396972ea461784a
oneday <youshihou5@gmail.com>
2020-09-08 18:36:16 +0800

    add Algorithm

---
+++ data-structure/Algorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Algorithm
+//
+//  Created by Ankui on 9/8/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
