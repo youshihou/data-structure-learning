a7e6a14a312d665a9eca861e3429a8c3a494bd1f
oneday <youshihou5@gmail.com>
2020-09-09 21:26:51 +0800

    add Hanoi

---
+++ data-structure/Algorithm/Recursion/Hanoi.h
@@ -0,0 +1,17 @@
+//
+//  Hanoi.h
+//  Algorithm
+//
+//  Created by Ankui on 9/9/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Hanoi : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/Algorithm/Recursion/Hanoi.m
@@ -0,0 +1,13 @@
+//
+//  Hanoi.m
+//  Algorithm
+//
+//  Created by Ankui on 9/9/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "Hanoi.h"
+
+@implementation Hanoi
+
+@end
