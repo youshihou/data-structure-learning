020850e1ca33b08fd1ce1de346343b1ce0b9c566
oneday <youshihou5@gmail.com>
2020-09-10 21:52:44 +0800

    add TailCall

---
+++ data-structure/Algorithm/recursion/TailCall.h
@@ -0,0 +1,17 @@
+//
+//  TailCall.h
+//  Algorithm
+//
+//  Created by Ankui on 9/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TailCall : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/Algorithm/recursion/TailCall.m
@@ -0,0 +1,13 @@
+//
+//  TailCall.m
+//  Algorithm
+//
+//  Created by Ankui on 9/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "TailCall.h"
+
+@implementation TailCall
+
+@end
