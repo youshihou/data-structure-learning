d57bd25c4148bd7d2cccd19716ecbb6075bef7ca
oneday <youshihou5@gmail.com>
2020-09-13 11:30:11 +0800

    add DivideConquer

---
+++ data-structure/Algorithm/divide-and-conquer/DivideConquer.h
@@ -0,0 +1,17 @@
+//
+//  DivideConquer.h
+//  Algorithm
+//
+//  Created by Ankui on 9/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DivideConquer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ data-structure/Algorithm/divide-and-conquer/DivideConquer.m
@@ -0,0 +1,13 @@
+//
+//  DivideConquer.m
+//  Algorithm
+//
+//  Created by Ankui on 9/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#import "DivideConquer.h"
+
+@implementation DivideConquer
+
+@end
