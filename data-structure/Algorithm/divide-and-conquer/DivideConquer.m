06e6e0c75fd2992d437281261cebdfd0a48c6cf6
oneday <youshihou5@gmail.com>
2020-09-13 12:17:31 +0800

    add DivideConquer

--- data-structure/Algorithm/divide-and-conquer/DivideConquer.m
+++ data-structure/Algorithm/divide-and-conquer/DivideConquer.m
@@ -25,7 +25,7 @@
     return max;
 }
 
-+ (NSInteger)maxSubarray:(NSArray *)array {
++ (NSInteger)maxSubarray2:(NSArray *)array {
     if (array.count == 0) { return 0; }
     NSInteger max = INT_MIN;
     NSInteger sum = 0;
@@ -37,5 +37,29 @@
         }
     }
     return max;
+}
+
++ (NSInteger)maxSubarray:(NSArray *)array {
+    if (array.count == 0) { return 0; }
+    return [self _maxSubarray:array begin:0 end:array.count];
+}
++ (NSInteger)_maxSubarray:(NSArray *)array begin:(NSInteger)begin end:(NSInteger)end {
+    if (end - begin < 2) { return [array[begin] integerValue]; }
+    NSInteger mid = (begin + end) >> 1;
+    NSInteger leftMax = INT_MIN;
+    NSInteger leftSum = 0;
+    for (NSInteger i = mid - 1; i >= begin; i--) {
+        leftSum += [array[i] integerValue];
+        leftMax = MAX(leftMax, leftSum);
+    }
+    NSInteger rightMax = INT_MIN;
+    NSInteger rightSum = 0;
+    for (NSInteger i = mid; i < end; i++) {
+        rightSum += [array[i] integerValue];
+        rightMax = MAX(rightMax, rightSum);
+    }
+    return MAX(leftMax + rightMax,
+               MAX([self _maxSubarray:array begin:begin end:mid],
+                   [self _maxSubarray:array begin:mid end:end]));
 }
 @end
