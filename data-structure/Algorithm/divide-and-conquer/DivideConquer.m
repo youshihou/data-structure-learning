857838ba3a3bdf1aaffb8279afb7820023e42acf
oneday <youshihou5@gmail.com>
2020-09-13 11:45:56 +0800

    add DivideConquer

--- data-structure/Algorithm/divide-and-conquer/DivideConquer.m
+++ data-structure/Algorithm/divide-and-conquer/DivideConquer.m
@@ -9,7 +9,7 @@
 #import "DivideConquer.h"
 
 @implementation DivideConquer
-+ (NSInteger)maxSubarray:(NSArray *)array {
++ (NSInteger)maxSubarray1:(NSArray *)array {
     if (array.count == 0) { return 0; }
     NSInteger max = INT_MIN;
     NSInteger sum = 0;
@@ -19,6 +19,20 @@
             for (NSInteger i = begin; i <= end; i++) {
                 sum += [array[i] integerValue];
             }
+            max = MAX(max, sum);
+        }
+    }
+    return max;
+}
+
++ (NSInteger)maxSubarray:(NSArray *)array {
+    if (array.count == 0) { return 0; }
+    NSInteger max = INT_MIN;
+    NSInteger sum = 0;
+    for (NSInteger begin = 0; begin < array.count; begin++) {
+        sum = 0;
+        for (NSInteger end = begin; end < array.count; end++) {
+            sum += [array[end] integerValue];
             max = MAX(max, sum);
         }
     }
